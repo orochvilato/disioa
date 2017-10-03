9683b983ca689371ee8074c1a37858ed95a4663a
orochvilato <olivierrochvilato@yahoo.com>
2017-10-03 15:53:58 +0200

    calcul compat pour groupe

--- controllers/administration.py
+++ controllers/administration.py
@@ -1,12 +1,40 @@
 # -*- coding: utf-8 -*-
 # essayez quelque chose comme
 import json
+def getPrivate():
+    name = request.args(0)
+    import os
+    f = open(os.path.join(request.folder,'private','data',name))
+    response.headers['ContentType'] ="application/octet-stream";
+    response.headers['Content-Disposition']="attachment; filename="+name
+    
+    return response.stream(f,chunk_size=4096)
+
+    
 def updateElectionTour():
     mdb.deputes.update_one({'depute_shortid':'anneblanc'},{'$set':{'depute_election.adversaires':[{'nom':"M. André AT (retiré)",'voix':0}]}})
     mdb.deputes.update_many({'depute_election.adversaires.0': { "$exists": False } }, {'$set':{'depute_election.tour':1}})
     mdb.deputes.update_many({'depute_election.adversaires.0': { "$exists": True } }, {'$set':{'depute_election.tour':2}})
 def test():
-    return BEAUTIFY(mdb.groupes.find_one({'groupe_abrev':'REM'})['stats'])
+    #return BEAUTIFY(mdb.deputes.find_one({'depute_uid':'PA719684'}))
+    gp_abrevs = mdb.groupes.distinct('groupe_abrev')
+    pgroup = dict((g+'_pour',{'$sum':'$depute_compat.'+g+'.pour'}) for g in ['FI','REM'])
+    pgroup.update(dict((g+'_total',{'$sum':'$depute_compat.'+g+'.total'}) for g in ['FI','REM']))
+    pgroup['_id'] = {'groupe':'$groupe_abrev'}
+    pipeline = [
+        {"$group": pgroup },
+    ]
+    compats = {}
+    
+    for cpt in mdb.deputes.aggregate(pipeline):
+        gp = cpt['_id']['groupe']
+        _comp = {}
+        for g in ['FI','REM']:
+            _comp[g] = {'pour':cpt[g+'_pour'],'total':cpt[g+'_total']}
+        compats[gp] = _comp
+    return BEAUTIFY(compats)
+    return BEAUTIFY(mdb.scrutins.find_one({'scrutin_id':'15_87'}))
+    
 def error():
     1/0
 def updateLogs():
--- controllers/visuels.py
+++ controllers/visuels.py
@@ -2,23 +2,67 @@
 # essayez quelque chose comme
 def index():
     from base64 import b64encode
+    import requests
     import os
     #depid = request.args(0)
-    deputes = list(mdb.deputes.find({'$and':[{'groupe_abrev':'REM'},{'stats.positions.exprimes':{'$lt':20}}]}))
+    deputes = list(mdb.deputes.find({'$and':[{'depute_actif':True},{'groupe_abrev':'REM'},{'stats.positions.exprimes':{'$lt':20}}]}))
     import random
-    depute = deputes[random.randint(0,len(deputes))]
-    
+    depute = deputes[random.randint(0,len(deputes)-1)]
+
     path = os.path.join(request.folder, 'static/images/deputes/%s.jpg' % depute['depute_id'])
-    photo = b64encode(open(path).read())
+    content = open(path).read()
+    #content =requests.get('http://www2.assemblee-nationale.fr/static/tribun/15/photos/'+depute['depute_uid'][2:]+'.jpg').content
+    photo = b64encode(content)
     s = depute['depute_nom'].split(' ')
     prenom = s[1]
     nom = ' '.join(s[2:])
-    return dict(message="hello from visuels.py",id=depute['depute_shortid'],photo=photo,nom=nom,prenom=prenom,pct="%.2f %%" % depute['stats']['positions']['exprimes'])
+    id = depute['depute_shortid']
+    pct = "%.2f %%" % depute['stats']['positions']['exprimes']
+    import re
+    svg = svgvisuel('test',photo=photo,prenom=prenom,nom=nom,pct=pct)
+    data = str(svg)
+    w,h = re.search(r'viewBox="0 0 ([0-9\.]+) ([0-9\.]+)"',data).groups()
+    w = float(w)
+    h = float(h)
+    coef = float(h/w)
+    return dict(coef=coef,id=id,photo=photo,nom=nom,prenom=prenom,pct=pct)
 
-def download():
+
+def getimage(deputeid,w,h):
+    
+    from selenium import webdriver
+    
+    driver = webdriver.PhantomJS(service_args=['--ignore-ssl-errors=true']) # or add to your PATH
+    driver.set_window_size(w,h)# optional
+    driver.get(URL('tweet',args=[deputeid],scheme=True, host=True))
+    data = driver.get_screenshot_as_png()
+    return data
+
+def tweeter():
+    from TwitterAPI import TwitterAPI
+    api = TwitterAPI('Ym8iy6cdZcr00D3cQ4I3nH6M5', 'OOMiTg1ezrX6aY8w5N6UMKMZe9BgmJLT6udnjfTEZ6NBOvlblm','3434108799-IzoPtfb1jPnMkStVZyDaDJ2Fa8iiBijfJMnNF99', 'MiMOaX9D8CYH7nIWPbJjlYgGHSUGTmKpwVH9FdV0GC6Rj')
+    id = request.args(0)
+    w = request.args(1)
+    h = request.args(2)
+    
+    data = getimage(id,w,h)
+    r = api.request('statuses/update_with_media', {'status':'post'}, {'media': data})
+    if r.status_code == 200:
+        picurl = r.json()['extended_entities']['media'][0]['display_url']
+        import urllib
+        params = urllib.urlencode({'text':'---Votre message---   '+picurl})
+        redirect('https://twitter.com/intent/tweet?'+params)
+   
+    #r = api.request('statuses/update_with_media', {'status':message},{'media[]':data})
+    #redirect('https://twitter.com/orochvilato?lang=fr')
+    return BEAUTIFY(r.json())
+    
+
+def tweet():
     from base64 import b64encode
     import os
     depid = request.args(0)
+    tweet = request.args(1)
     depute = mdb.deputes.find_one({'depute_shortid':depid})
     if not depute:
         depute = mdb.deputes.find_one({'depute_shortid':'jeanlucmelenchon'})
@@ -29,11 +73,38 @@ def download():
     prenom = s[1]
     nom = ' '.join(s[2:])
     pct="%.2f %%" % depute['stats']['positions']['exprimes']
-    inputsvg = str(svgvisuel('test',photo=photo,prenom=prenom,nom=nom,pct=pct))
-    #result = cairosvg.svg2png(inputsvg,write_to=os.path.join(request.folder,'static/test.png'))
-    return XML(inputsvg)
+    return dict(id=depute['depute_shortid'],photo=photo,nom=nom,prenom=prenom,pct="%.2f %%" % depute['stats']['positions']['exprimes'])
+
+def download():
+    from base64 import b64encode
+    import os
+    depid = request.args(0)
+    width = request.args(1)
+    height = request.args(2)
+   
+    depute = mdb.deputes.find_one({'depute_shortid':depid})
+    if not depute:
+        depute = mdb.deputes.find_one({'depute_shortid':'jeanlucmelenchon'})
+
+    path = os.path.join(request.folder, 'static/images/deputes/%s.jpg' % depute['depute_id'])
+    photo = b64encode(open(path).read())
+    s = depute['depute_nom'].split(' ')
+    prenom = s[1]
+    nom = ' '.join(s[2:])
+    pct="%.2f %%" % depute['stats']['positions']['exprimes']
+    
+    if width:
+        data = getimage(depid,int(width),int(height))
+        compl = "%sx%s" % (width,height)
+        ext = 'png'
+    else:
+        svg = svgvisuel('test',photo=photo,prenom=prenom,nom=nom,pct=pct)
+        data = str(svg)
+        compl = ""
+        ext = "svg"
+        
     from cStringIO import StringIO
     response.headers['ContentType'] ="application/octet-stream";
-    response.headers['Content-Disposition']="attachment; filename="+depute['depute_shortid']+".svg"
-    
-    return response.stream(StringIO(inputsvg),chunk_size=4096)
+    response.headers['Content-Disposition']="attachment; filename="+depute['depute_shortid']+compl+"."+ext
+
+    return response.stream(StringIO(data),chunk_size=4096)
